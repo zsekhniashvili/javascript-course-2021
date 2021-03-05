f75056499e6758614e74b5afdbf9871985e53b59
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-03-05 10:55:49 +0400

    Add initial files

---
+++ lesson1/app.js
@@ -0,0 +1,24 @@
+// alert, confirm, prompt
+
+// console
+
+// Accessing DOM, change innerHTML, change color
+
+// Writing in DOM. document.write
+
+// variables, types. let vs const vs var
+
+// block scope. difference between let and var
+
+// comments
+
+// strings/string methods
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// conditionals, ternary
+
+// loops (while, do-while, for)
+
+// functions
+
+// Write program to calculate n factorial
---
+++ lesson1/homework.js
@@ -0,0 +1,5 @@
+// 1. Write a recursive function which calculates n factorial
+
+// 2. Write a recursive function which asks name for user using prompt and keeps asking until user enters proper name. Proper name: at least 4 characters
+
+// 3. 
