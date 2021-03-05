4a53329df10a2ac589259a7f7802656e1d2aea61
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-03-05 11:54:28 +0400

    Prepare homework

--- lesson1/homework.js
+++ lesson1/homework.js
@@ -1,5 +1,3 @@
-// 1. Write a recursive function which calculates n factorial
+// 1. Write a recursive function which calculates n factorial. Draw a table which will have two columns: N and factorial of N
 
 // 2. Write a recursive function which asks name for user using prompt and keeps asking until user enters proper name. Proper name: at least 4 characters
-
-// 3. 
