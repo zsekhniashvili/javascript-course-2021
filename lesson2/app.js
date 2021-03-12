ce06f7ffc1e1e6b41d04cf4a786f26ecd7201ba5
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-03-12 08:55:40 +0400

    Add lesson2 start content

---
+++ lesson2/app.js
@@ -0,0 +1,50 @@
+// =========================
+// Arrays
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+// =========================
+
+// Creating Arrays
+
+// Accessing elements of the array
+
+// Array length
+
+// Inserting into array
+
+// Removing from array
+
+// Iterating in array
+
+// Practice #1: find the sum of the elements of the array
+const numbers = [4, 3, 8, 2, 9, 12, 7, 6];
+
+// Array methods: push, pop, shift, unshift, slice, splice, concat, etc...
+
+// Practice #2: You have two arrays, append values from the first array at the end of the second array
+const firstArray = [7, 8, 9];
+const secondArray = [1, 2, 3, 4, 5, 6];
+
+// ==========================
+// Objects
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+// ==========================
+
+// Creating Objects
+
+// Set & get & delete properties, access properties using []
+
+// Iterating object
+
+// Print object keys, values
+
+// Arrays of objects
+
+// JSON
+
+// Destructuring, function arguments, spread, rest
+
+// High order functions; filter, map, reduce
+/**
+ * https://youtu.be/rRgD1yVwIvE
+ * https://youtu.be/-lNgE-WjuXY
+ */
---
+++ lesson2/homework.js
@@ -0,0 +1,13 @@
+const employees = [
+    { name: 'Zura', salary: 1500, age: 28 },
+    { name: 'George', salary: 2000, age: 22 },
+    { name: 'Anna', salary: 1800, age: 23 },
+    { name: 'David', salary: 2500, age: 21 },
+    { name: 'Brad', salary: 2600, age: 33 },
+];
+
+// Calculate average salary for all employees
+
+// Find youngest employee
+
+// Create new array of employees whose salary is greater than or equal 2000
