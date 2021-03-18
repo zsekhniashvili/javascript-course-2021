6658b7188a16a56efc049ee8554dcf7e6ad571e9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-03-18 09:26:08 +0400

    Prepare code for lesson2 start

--- lesson2/app.js
+++ lesson2/app.js
@@ -4,25 +4,51 @@
 // =========================
 
 // Creating Arrays
+// ========================================
 
-// Accessing elements of the array
+// Get Elements of the array
+// ========================================
+
+// Set Elements of the array
+// ========================================
 
 // Array length
+// ========================================
 
-// Inserting into array
-
-// Removing from array
+// Deleting array elements
+// ========================================
 
 // Iterating in array
+// ========================================
+
 
 // Practice #1: find the sum of the elements of the array
-const numbers = [4, 3, 8, 2, 9, 12, 7, 6];
+// ========================================
+// const numbers = [4, 3, 8, 2, 9, 12, 7, 6];
 
 // Array methods: push, pop, shift, unshift, slice, splice, concat, etc...
 
+
+// Add values
+// ========================================
+
+// Remove values
+// ========================================
+
+// insert first
+// ========================================
+
+// Remove from the beginning
+// ========================================
+
+// Removing elements from the index of the array
+// ========================================
+
+// Inserting elements at specific position
+// ========================================
+
 // Practice #2: You have two arrays, append values from the first array at the end of the second array
-const firstArray = [7, 8, 9];
-const secondArray = [1, 2, 3, 4, 5, 6];
+// ========================================
 
 // ==========================
 // Objects
@@ -30,20 +56,42 @@ const secondArray = [1, 2, 3, 4, 5, 6];
 // ==========================
 
 // Creating Objects
+// =======================================
 
 // Set & get & delete properties, access properties using []
+// =======================================
+
+// Access by []
+// =======================================
 
 // Iterating object
+// =======================================
 
-// Print object keys, values
+// Deleting properties
+// =======================================
+
+// Object keys & Object values
+// =======================================
 
 // Arrays of objects
+// =======================================
 
-// JSON
+// JSON -> Javascript Object Notation
+// =========================================
 
-// Destructuring, function arguments, spread, rest
+
+// Destructuring, function arguments
+// =========================================
+
+// Rest operator
+// =========================================
+
+
+// Spread operator
+// ==========================================
 
 // High order functions; filter, map, reduce
+// ========================================
 /**
  * https://youtu.be/rRgD1yVwIvE
  * https://youtu.be/-lNgE-WjuXY
