cd93770fb31c6087055d2301b8ff93b1a3e55b81
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-03-19 11:42:12 +0400

    Add homework.js

---
+++ lesson3/homework.js
@@ -0,0 +1,34 @@
+// Email Subject: "Web II - 2021 - Pracical 1"
+
+// =======================================
+// Challenge 1
+// =======================================
+
+// Create a class Shape with color property and generate constructor
+
+// Create a class Circle with radius and extend it from Shape  and generate constructor
+
+// Create a method in Circle class which calculates and returns area of the circle
+
+// Create an instance of the Circle class and print the result of that method
+
+
+// =======================================
+// Challenge 2
+// =======================================
+
+// Create Student class with name, personalId and age
+
+// Create Subject class with name and code
+
+// Create University class with name, students array and subjects array
+
+/* Add three methods in university class: 
+     1) addStudent(student) - which accepts Student instance and adds in students array
+     2) addSubject(subject) - which accepts Subject instance and adds in subjects array
+     3) chooseSubject(personalId, subjectCode) - which will choose the subject for the student and save in student subjects. 
+    This must print "invalid personalId" if student does not exist with this personal ID in university.
+    This must print "invalid subject code" if subject does not exist with this subject code in university.
+     4) getSubjectsForStudent(personalId) - This will return chosne subjects for given student
+*/
+
