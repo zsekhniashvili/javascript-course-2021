e49ca4890f413611362d073e8f9d4ec35cad8cea
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-04-09 12:28:50 +0400

    Finish lesson5

---
+++ lesson5/homework.js
@@ -0,0 +1,53 @@
+// =======================================
+// Challenge 1
+// =======================================
+
+// Form validation plugin
+
+
+// =======================================
+// Challenge 2
+// =======================================
+
+// Create Context menu
+
+
+// =======================================
+// Challenge 3
+// =======================================
+
+// Picture puzzle game
+
+
+// =======================================
+// Challenge 4
+// =======================================
+
+// Dragging ball
+
+
+// =======================================
+// Must do challenge
+// =======================================
+
+const words = [
+    {
+        word: 'Iphone 11', 
+        image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-11-pro-select-2019-family_GEO_EMEA?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1567812929188',
+        description: 'The best phone'
+    },
+    {
+        word: 'messi', 
+        description: 'Messi has 40M EUR salary in a year'
+    },
+    {
+        word: 'Elon Musk', 
+        description: 'Elon Musk lost $16B'
+    },
+];
+
+// Traverse DOM and show texts on mouse over for words.
+
+// =======================================
+// Bonus: Create Calculator
+// =======================================
