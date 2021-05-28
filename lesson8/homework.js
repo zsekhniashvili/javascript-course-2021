3e49c3ced987f011aba6bfe44f81800f74e25fa0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-05-28 11:43:57 +0400

    Lesson 8 code

---
+++ lesson8/homework.js
@@ -0,0 +1,18 @@
+// Icons
+// https://fontawesome.com/icons?d=gallery
+
+// =======================================
+// Card Memory Game
+// =======================================
+
+// Project  - https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Card-Memory-Game.md
+// Example  - https://codepen.io/zerospree/full/bNWbvW
+
+
+// =======================================
+// Book Searching App
+// =======================================
+
+// Project  - https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Book-Finder-App.md
+// Example  - https://book-finder-by-deyl.netlify.app/
+// API      - https://developers.google.com/books
---
+++ lesson8/module.js
@@ -0,0 +1,3 @@
+import * as math from "./math.js";
+
+console.log(math.PI, math.addition(1, 2));
