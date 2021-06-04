2b27be9fe2370237a197b25093374a814abc95bf
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-06-04 12:29:41 +0400

    Final version

---
+++ lesson9/homework.js
@@ -0,0 +1,24 @@
+// ========================================================
+// Create image resizer using vanilla Javascript and canvas
+// https://dev.to/taylorbeeston/resizing-images-client-side-with-vanilla-js-4ng2
+// ========================================================
+
+// ========================================================
+// Create analog clock using canvas
+// https://www.w3schools.com/graphics/canvas_clock_start.asp
+// https://i2.wp.com/www.compu-gen.com/wp-content/uploads/2015/09/OnTime-Analog-Clock.png?resize=570%2C301
+// ========================================================
+
+// ========================================================
+// Create paint app using canvas
+// ========================================================
+
+// ========================================================
+// Draw fractal tree
+// https://gist.github.com/gsluthra/3401766
+// ========================================================
+
+// ========================================================
+// Understand how this game works and write on your own
+// https://www.w3schools.com/graphics/game_intro.asp
+// ========================================================
