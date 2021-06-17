862d73f88a32dcae178655f07d4fbb4a7c94e665
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-06-17 23:49:41 +0400

    Initial commit for lesson11

---
+++ lesson11/app.js
@@ -0,0 +1,22 @@
+// What is npm
+
+// Create an npm package
+
+// Install bootstrap using npm
+
+// Getting started with webpack: npm install webpack webpack-cli --save-dev
+// https://webpack.js.org/guides/getting-started/
+
+// Use import/export syntax with webpack
+
+// Loading styles, CSS
+
+// Compile scss into css
+
+// Compile typescript into Javascript
+
+// Loading Images, fonts
+
+// Loading CSV, XML
+
+// Create sample project
